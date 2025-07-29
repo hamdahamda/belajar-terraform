1ba3b808559406e22c7645a4154f5624ced0d9ce
Ilhamda <ilhamda19@gmail.com>
2025-07-29 13:33:58 +0700

    create 2 vm with terraform

---
+++ 2. Create 2 VM/main.tf
@@ -0,0 +1,44 @@
+provider "google" {
+  project     = var.project_id
+  region      = var.region
+  zone        = var.zone
+  credentials = file(var.credentials_path)
+}
+
+resource "google_compute_instance" "web_server" {
+  name         = "web-server"
+  machine_type = "e2-micro"
+  zone         = var.zone
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-12"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {}
+  }
+
+  tags = ["web"]
+}
+
+resource "google_compute_instance" "db_server" {
+  name         = "db-server"
+  machine_type = "e2-standard-2"
+  zone         = var.zone
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-12"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {}
+  }
+
+  tags = ["db"]
+}
