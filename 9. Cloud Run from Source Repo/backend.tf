26f21ac813ab8b69b50002eebb8466fa6e53bfd0
Ilhamda <ilhamda19@gmail.com>
2025-07-31 13:55:43 +0700

    update name module

--- 9. Cloud Run from Source Repo/backend.tf
+++ 9. Cloud Run from Source Repo/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "gcs" {
     bucket  = "my-terraform-state-bucket-00"  # Ganti sesuai nama bucket kamu
-    prefix = "terraform/step-8"                       # Folder logis di dalam bucket
+    prefix = "terraform/step-9"                       # Folder logis di dalam bucket
   }
 }
