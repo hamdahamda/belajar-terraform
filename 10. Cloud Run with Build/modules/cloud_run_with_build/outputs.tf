99c155640a46046bcde368b437eb64d6f3350e7b
Ilhamda <ilhamda19@gmail.com>
2025-08-01 14:29:17 +0700

    add step 10

--- 10. Cloud Run with Build/modules/cloud_run_with_build/outputs.tf
+++
@@ -1,7 +0,0 @@
-# output "service_name" {
-#   value = google_cloud_run_service.default.name
-# }
-
-output "trigger_id" {
-  value = google_cloudbuild_trigger.default.id
-}
--- 10. Cloud Run with Build/modules/cloud_run_with_build/variables.tf
+++
@@ -1,7 +0,0 @@
-variable "project_id" {}
-variable "region" {}
-variable "image_name" {}
-variable "service_name" {}
-variable "github_owner" {}
-variable "github_repo" {}
-
