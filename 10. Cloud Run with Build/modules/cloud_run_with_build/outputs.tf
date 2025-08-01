be741848e928d548603c8d448e7ab25db9ae76d4
Ilhamda <ilhamda19@gmail.com>
2025-08-01 14:59:33 +0700

    add step 10

--- 10. Cloud Run with Build/modules/cloud_run_with_build/outputs.tf
+++ 10. Cloud Run with Build/modules/cloud_run_with_build/outputs.tf
@@ -2,6 +2,6 @@
 #   value = google_cloud_run_service.default.name
 # }
 
-output "trigger_id" {
-  value = google_cloudbuild_trigger.default.id
-}
+# output "trigger_id" {
+#   value = google_cloudbuild_trigger.default.id
+# }
