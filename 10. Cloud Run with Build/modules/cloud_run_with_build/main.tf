c5c6e8e26ba15bd61762cb6c50bfdc8b7729bab1
Ilhamda <ilhamda19@gmail.com>
2025-08-01 14:58:44 +0700

    add step 10

--- 10. Cloud Run with Build/modules/cloud_run_with_build/main.tf
+++ 10. Cloud Run with Build/modules/cloud_run_with_build/main.tf
@@ -1,19 +1,34 @@
-resource "google_cloudbuild_trigger" "default" {
-  name        = "trigger-${var.service_name}"
-  description = "Trigger for ${var.service_name}"
+# resource "google_cloudbuild_trigger" "default" {
+#   name        = "trigger-${var.service_name}"
+#   description = "Trigger for ${var.service_name}"
 
+#   github {
+#     owner = var.github_owner
+#     name  = var.github_repo
+#     push {
+#       branch = "^main$"
+#     }
+#   }
+
+#   filename = "cloudbuild.yml"
+#   substitutions = {
+#     _SERVICE_NAME = var.service_name
+#     _IMAGE_NAME   = var.image_name
+#     _REGION       = var.region
+#   }
+# }
+
+
+resource "google_cloudbuild_trigger" "test_minimal" {
+  name = "test-trigger"
+  
   github {
-    owner = var.github_owner
-    name  = var.github_repo
+    owner = "hamdahamda"
+    name  = "latihan-css-web"
     push {
       branch = "^main$"
     }
   }
 
   filename = "cloudbuild.yml"
-  substitutions = {
-    _SERVICE_NAME = var.service_name
-    _IMAGE_NAME   = var.image_name
-    _REGION       = var.region
-  }
 }
