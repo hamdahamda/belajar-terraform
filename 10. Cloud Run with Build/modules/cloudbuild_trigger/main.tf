1b3e11cb5f993b2e4ac2d39cb5acd5700bc0845a
Ilhamda <ilhamda19@gmail.com>
2025-08-02 23:48:41 +0700

    add step 10

--- 10. Cloud Run with Build/modules/cloudbuild_trigger/main.tf
+++ 10. Cloud Run with Build/modules/cloudbuild_trigger/main.tf
@@ -3,12 +3,48 @@ resource "google_cloudbuild_trigger" "this" {
   description = "Build and deploy from GitHub"
 
   github {
-    owner      = var.github_owner
-    name       = var.github_repo
+    owner = var.github_owner
+    name  = var.github_repo
     push {
       branch = var.github_branch
     }
   }
 
-  filename = "cloudbuild.yaml"
+  build {
+    step {
+      name = "gcr.io/cloud-builders/docker"
+      args = [
+        "build", "-t", var.image_url, "."
+      ]
+    }
+
+    step {
+      name = "gcr.io/cloud-builders/docker"
+      args = ["push", var.image_url]
+    }
+
+    step {
+      name       = "gcr.io/google.com/cloudsdktool/cloud-sdk"
+      entrypoint = "gcloud"
+      args = [
+        "run", "deploy", var.service_name,
+        "--image", var.image_url,
+        "--region", var.region,
+        "--platform", "managed",
+        "--allow-unauthenticated"
+      ]
+    }
+
+    images = [var.image_url]
+
+    # ✅ FIX: Tambahkan opsi logging agar build tidak error
+    options {
+      logging = "CLOUD_LOGGING_ONLY"
+    }
+  }
+
+  # (Opsional jika kamu menggunakan SA tertentu)
+  # substitutions = {
+  #   _SERVICE_ACCOUNT = var.build_sa_email
+  # }
 }
--- 10. Cloud Run with Build/modules/cloudbuild_trigger/variables.tf
+++ 10. Cloud Run with Build/modules/cloudbuild_trigger/variables.tf
@@ -1,16 +1,41 @@
 variable "trigger_name" {
-    default = "cloud-run-trigger-testing"
+  description = "Name of the Cloud Build trigger"
+  type        = string
+  default     = "cloud-run-trigger-testing"
 }
-variable "github_owner" {}
-variable "github_repo" {}
+
+variable "github_owner" {
+  description = "GitHub repository owner"
+  type        = string
+}
+
+variable "github_repo" {
+  description = "GitHub repository name"
+  type        = string
+}
+
 variable "github_branch" {
-default = "^main$"
+  description = "GitHub branch to trigger builds from"
+  type        = string
+  default     = "^main$"
 }
+
 variable "image_url" {
-  default = "us-central1-docker.pkg.dev/sodium-pattern-465207-m9/cloudrun-testing-with-terraform/app:latest"
+  description = "URL of the container image"
+  type        = string
+  default     = "us-central1-docker.pkg.dev/sodium-pattern-465207-m9/cloudrun-testing-with-terraform/app:latest"
 }
-variable "service_name" {}
-variable "region" {}
+
+variable "service_name" {
+  description = "Cloud Run service name"
+  type        = string
+}
+
+variable "region" {
+  description = "GCP region"
+  type        = string
+}
+
 variable "project_id" {
   description = "The GCP project ID"
   type        = string
