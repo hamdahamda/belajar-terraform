5bb9797d2fba54ee0ca1da531b4cb6835cf0615e
Ilhamda <ilhamda19@gmail.com>
2025-08-02 21:34:21 +0700

    fixing err

--- 10. Cloud Run with Build/backend.tf
+++
@@ -1,6 +0,0 @@
-terraform {
-  backend "gcs" {
-    bucket  = "my-terraform-state-bucket-00"  # Ganti sesuai nama bucket kamu
-    prefix = "terraform/step-10"                       # Folder logis di dalam bucket
-  }
-}
--- 10. Cloud Run with Build/output.tf
+++
@@ -1,4 +0,0 @@
-
-output "cloud_run_url" {
-  value = "Cloud Run deployed via Cloud Build trigger. URL will appear in GCP console."
-}
