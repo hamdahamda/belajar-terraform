876d9d8eb992c43f185b5228cf2b47680a56e481
Ilhamda <ilhamda19@gmail.com>
2025-08-02 23:34:58 +0700

    add step 10

--- 10. Cloud Run with Build/modules/cloudbuild_trigger/main.tf
+++ 10. Cloud Run with Build/modules/cloudbuild_trigger/main.tf
@@ -1,42 +1,14 @@
 resource "google_cloudbuild_trigger" "this" {
   name        = var.trigger_name
   description = "Build and deploy from GitHub"
-  project     = var.project_id
 
   github {
     owner = var.github_owner
     name  = var.github_repo
-
     push {
       branch = var.github_branch
     }
   }
 
-  build {
-    timeout = "900s"
-
-    step {
-      name = "gcr.io/cloud-builders/docker"
-      args = ["build", "-t", var.image_url, "."]
-    }
-
-    step {
-      name = "gcr.io/cloud-builders/docker"
-      args = ["push", var.image_url]
-    }
-
-    step {
-      name       = "gcr.io/google.com/cloudsdktool/cloud-sdk"
-      entrypoint = "gcloud"
-      args = [
-        "run", "deploy", var.service_name,
-        "--image", var.image_url,
-        "--region", var.region,
-        "--platform", "managed",
-        "--allow-unauthenticated"
-      ]
-    }
-
-    images = [var.image_url]
-  }
+  filename = "cloudbuild.yaml"  # Ini akan mencari file di repo GitHub kamu
 }
--- 10. Cloud Run with Build/modules/cloudbuild_trigger/variables.tf
+++ 10. Cloud Run with Build/modules/cloudbuild_trigger/variables.tf
@@ -4,7 +4,7 @@ variable "trigger_name" {
 variable "github_owner" {}
 variable "github_repo" {}
 variable "github_branch" {
-default = "main"
+default = "^main$"
 }
 variable "image_url" {
   default = "us-central1-docker.pkg.dev/sodium-pattern-465207-m9/cloudrun-testing-with-terraform/app:latest"
