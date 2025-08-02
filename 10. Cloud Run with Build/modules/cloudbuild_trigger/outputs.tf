68c5850855c6a0b11319afbcaa59ff56d3990caa
Ilhamda <ilhamda19@gmail.com>
2025-08-02 23:07:13 +0700

    add step 10

--- 10. Cloud Run with Build/modules/cloudbuild_trigger/outputs.tf
+++ 10. Cloud Run with Build/modules/cloudbuild_trigger/outputs.tf
@@ -1,7 +1,11 @@
-output "cloudbuild_trigger_config" {
+output "debug_trigger" {
   value = {
-    owner   = var.github_owner
-    repo    = var.github_repo
-    branch  = var.github_branch
+    trigger_name   = var.trigger_name
+    github_owner   = var.github_owner
+    github_repo    = var.github_repo
+    github_branch  = var.github_branch
+    image_url      = var.image_url
+    service_name   = var.service_name
+    region         = var.region
   }
 }
