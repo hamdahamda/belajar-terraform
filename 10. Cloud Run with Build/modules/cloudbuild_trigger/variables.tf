5d5e8f0f1990a75f7833d5df65bad9c1fdf2765a
Ilhamda <ilhamda19@gmail.com>
2025-08-02 23:23:22 +0700

    add step 10

--- 10. Cloud Run with Build/modules/cloudbuild_trigger/variables.tf
+++ 10. Cloud Run with Build/modules/cloudbuild_trigger/variables.tf
@@ -6,11 +6,12 @@ variable "github_repo" {}
 variable "github_branch" {
 default = "main"
 }
-
 variable "image_url" {
   default = "us-central1-docker.pkg.dev/sodium-pattern-465207-m9/cloudrun-testing-with-terraform/app:latest"
 }
-
-
 variable "service_name" {}
 variable "region" {}
+variable "project_id" {
+  description = "The GCP project ID"
+  type        = string
+}
