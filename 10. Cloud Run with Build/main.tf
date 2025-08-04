dee38da493b197a8992dd01e1e6388a3d3f04367
Ilhamda <ilhamda19@gmail.com>
2025-08-04 21:09:15 +0700

    update modul

--- 10. Cloud Run with Build/main.tf
+++ 10. Cloud Run with Build/main.tf
@@ -9,14 +9,14 @@ module "artifact_registry" {
   region    = var.region
 }
 
-module "cloudbuild_trigger" {
-  source         = "./modules/cloudbuild_trigger"
-  trigger_name   = "auto-deploy-${var.service_name}"
-  github_owner   = var.github_owner
-  github_repo    = var.github_repo
-  github_branch  = "main"
-  image_url      = "${var.region}-docker.pkg.dev/${var.project_id}/${var.repo_name}/${var.image_name}"
-  service_name   = var.service_name
-  region         = var.region
-  project_id    = var.project_id
-}
+# module "cloudbuild_trigger" {
+#   source         = "./modules/cloudbuild_trigger"
+#   trigger_name   = "auto-deploy-${var.service_name}"
+#   github_owner   = var.github_owner
+#   github_repo    = var.github_repo
+#   github_branch  = "main"
+#   image_url      = "${var.region}-docker.pkg.dev/${var.project_id}/${var.repo_name}/${var.image_name}"
+#   service_name   = var.service_name
+#   region         = var.region
+#   project_id    = var.project_id
+# }
